b962a410174aa2369df983db7f4aed33620a8f41
icodo98 <rsb98759@gmail.com>
2023-01-03 02:17:12 +0900

    Found item codes and json body

--- src/LOAT/JUnitTest.java
+++ src/LOAT/JUnitTest.java
@@ -7,10 +7,16 @@ import java.io.File;
 
 public class JUnitTest {
     @Test
-    public void test(){
+    public void test() {
         JSONObject jo = new JSONObject(Utils.readContentsAsString(new File("MarketOptions")));
 
         System.out.println(jo.keys());
+    }
+
+    @Test
+    public void JSonbuilderTest() {
+        Repository r = new Repository();
+        JSONObject jo = r.requestItem("GRADE", 0, "ee", 2, "ee", "ee", 0, "ASC");
     }
 
 }
--- src/LOAT/Repository.java
+++ src/LOAT/Repository.java
@@ -1,10 +1,49 @@
 package LOAT;
 
+import org.json.JSONObject;
+
 import java.net.URL;
 
 public class Repository {
-    public void MARKET(String Ops, URL URL){
-        //생활 카테고리 90000, 재련재료(오레하 포함해서 검색) 50010
+    public JSONObject requestItem(String Sort, int CategoryCode, String CharacterClass, int ItemTier, String ItemGrade,
+            String ItemName, int PageNo, String SortCondition) {
+        // JSONObject result = new JSONObject("{\n" +
+        // " \"Sort\": \"GRADE\",\n" +
+        // " \"CategoryCode\": 0,\n" +
+        // " \"CharacterClass\": \"string\",\n" +
+        // " \"ItemTier\": 0,\n" +
+        // " \"ItemGrade\": \"string\",\n" +
+        // " \"ItemName\": \"string\",\n" +
+        // " \"PageNo\": 0,\n" +
+        // " \"SortCondition\": \"ASC\"\n" +
+        // "}");
+        JSONObject result = new JSONObject();
+        result.append("Sort", Sort);
+        result.append("CategoryCode", CategoryCode);
+        result.append("CharacterClass", CharacterClass);
+        result.append("ItemTier", ItemTier);
+        result.append("ItemGrade", ItemGrade);
+        result.append("ItemName", ItemName);
+        result.append("PageNo", PageNo);
+        result.append("SortCondition", SortCondition);
+
+        result.put("Sort", Sort);
+        result.put("CategoryCode", CategoryCode);
+        result.put("CharacterClass", CharacterClass);
+        result.put("ItemTier", ItemTier);
+        result.put("ItemGrade", ItemGrade);
+        result.put("ItemName", ItemName);
+        result.put("PageNo", PageNo);
+        result.put("SortCondition", SortCondition);
+
+        return result;
+    }
+
+    public void MARKET(String Ops, URL URL) {
+        // 생활 카테고리 90000, 재련재료(오레하 포함해서 검색) 50010
+        // 검색해야할것.
+        // 오레하 id 6861007~6861011 (10은 칼디르)
 
     }
+
 }
